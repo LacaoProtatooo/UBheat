acf1e8e74c5dd3ef50753f000652a43e66bb1815
Protatooo <133950548+LacaoProtatooo@users.noreply.github.com>
2025-03-10 04:53:40 +0800

    Revert "wtf"
    This reverts commit 4b8927cf660fb4c23593a98a378a1994e3a68da4.

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ import authRoutes from "./routes/authRoutes.js";
 // -------------
 const app = express();
 
-// Middleware for parsing request bodies /
+// Middleware for parsing request bodies
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
