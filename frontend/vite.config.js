2e4e2f532b1c474355ea611591dcb272636f1dc8
Protatooo <133950548+LacaoProtatooo@users.noreply.github.com>
2025-03-16 17:36:05 +0800

    umayyy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:5000";
-
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': apiUrl,
+      '/api': import.meta.env.VITE_API_URL,
     },
   },
   build: {
