c8132a11b77090a78b9c17a37d220b12f1141ee7
Protatooo <133950548+LacaoProtatooo@users.noreply.github.com>
2025-03-24 08:32:32 +0800

    color ng card

--- frontend/src/components/ui/CardHenrich.jsx
+++ frontend/src/components/ui/CardHenrich.jsx
@@ -5,10 +5,8 @@ export const CardHenrich = ({ className, children }) => {
   return (
     <div
       className={cn(
-        "group w-full cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end p-4 border border-transparent dark:border-neutral-800",
-        "bg-[url(https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcHVoNGRoaWsxNmRyb3VzMGNwZ3lwNzFmb3A1MWlnMTU4MXBteGhpNSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/bGgsc5mWoryfgKBx1u/giphy.gif)] bg-cover",
-        "before:bg-[url(https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcHVoNGRoaWsxNmRyb3VzMGNwZ3lwNzFmb3A1MWlnMTU4MXBteGhpNSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/26tn33aiTi1jkl6H6/giphy.gif)] before:fixed before:inset-0 before:opacity-0 before:z-[-1]",
-        "hover:bg-[url(https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcHVoNGRoaWsxNmRyb3VzMGNwZ3lwNzFmb3A1MWlnMTU4MXBteGhpNSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/26tn33aiTi1jkl6H6/giphy.gif)]",
+        "group w-full cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end p-4 border border-slate-700 dark:border-neutral-800",
+        "bg-slate-700",
         "hover:after:content-[''] hover:after:absolute hover:after:inset-0 hover:after:bg-black hover:after:opacity-50",
         "transition-all duration-500",
         className
--- frontend/src/components/ui/card.jsx
+++ frontend/src/components/ui/card.jsx
@@ -6,9 +6,7 @@ export const Card = ({ className, children }) => {
     <div
       className={cn(
         "group w-full cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end p-4 border border-transparent dark:border-neutral-800",
-        "bg-[url(https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExODY1NHJjYTZsa2hhdTQ5dnBhejlxYzB0eWdjaWs3NjhkcDFxbmxxMSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/2IudUHdI075HL02Pkk/giphy.gif)] bg-cover",
-        "before:bg-[url(https://media.giphy.com/media/HzPtbOKyBoBFsK4hyc/giphy.gif?cid=790b76118654rca6lkhau49vpaz9qc0tygcik768dp1qnlq1&ep=v1_gifs_search&rid=giphy.gif&ct=g)] before:fixed before:inset-0 before:opacity-0 before:z-[-1]",
-        "hover:bg-[url(https://media.giphy.com/media/HzPtbOKyBoBFsK4hyc/giphy.gif?cid=790b76118654rca6lkhau49vpaz9qc0tygcik768dp1qnlq1&ep=v1_gifs_search&rid=giphy.gif&ct=g)]",
+        "bg-slate-700",
         "hover:after:content-[''] hover:after:absolute hover:after:inset-0 hover:after:bg-black hover:after:opacity-50",
         "transition-all duration-500",
         className
